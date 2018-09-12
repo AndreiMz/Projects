695cc2c7b6436044c136137c63de2b382eab9edd
AndreiMz <manzala.andrei@gmail.com>
2018-09-12 10:36:32 +0300

    More design consistency + treated exception in home page
    Would throw nil.nilclass exception if random generated a deleted ID,
    now it just doesn't render a video.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,16 @@
 # default by rails
 class HomeController < ApplicationController
   def index
-    if Video.count > 12
-      rands = Array.new(12) { rand(1..Video.last.id) }
+    if Video.count > 6
+      rands = (1..Video.last.id).sort{ rand() - 0.5 }[0..5]
       @videos = []
       rands.each do |t|
+        begin
         x = Yt::Video.new id: Video.find(t).youtube_id
         @videos << x
+      rescue ActiveRecord::RecordNotFound
+        next
+        end
       end
     end
   end
